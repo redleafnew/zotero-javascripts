0f1a486e87c110601f4a6966c6a517f201f76304
redleafnew <redleafnew@163.com>
2021-12-04 16:57:06 +0800

    可以选择引用哪个次数

--- 14extra-replace-citations.js
+++ 14extra-replace-citations.js
@@ -2,7 +2,8 @@ zoteroPane = Zotero.getActiveZoteroPane();
 items = zoteroPane.getSelectedItems();
 for (item of items) {
     var oextra = item.getField("extra");
-    var rep = oextra.match(/\d+ citations/)[0]
+    var rep = oextra.match(/(\d+ citations)(.+\n)(\d+ citations)(.+)/)[1]
+    //第1个引用次数写为[1]，第2个引用次数，写为[3]
     item.setField("extra", rep);
     await item.saveTx();
 }

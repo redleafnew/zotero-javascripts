192a2b4c2809906625f6f70cb18edf3613651955
redleafnew <redleafnew@163.com>
2022-01-04 13:59:51 +0800

    修复多篇连到一起的问题，现在多篇可正常显示

--- 16copy-creators-to-clipboard.js
+++ 16copy-creators-to-clipboard.js
@@ -1,31 +1,30 @@
 
-// 单篇可行 
-// 多篇会连到一起
 var zoteroPane = Zotero.getActiveZoteroPane();
 var zitems = zoteroPane.getSelectedItems();
+var clipboardText = ''
 
-
-var creatorsArray = [];
 for (let zitem of zitems) {
+    var creatorsArray = [];
     var whiteSpace = ' ';
     var lan = zitem.getField("language");
     if (lan.indexOf("中") != -1)  //如果为中文
      { whiteSpace = ''; //如果为中文删除空格
         }
-    let creators = zitem.getCreators()
+    let creators = zitem.getCreators();
     for (let creator of creators) {
-        let creatorStr = creator.lastName + whiteSpace + creator.firstName
+        let creatorStr = creator.lastName + whiteSpace + creator.firstName;
         if (creatorsArray.indexOf(creatorStr) == -1) {
-            creatorsArray.push(creatorStr)
+            creatorsArray.push(creatorStr);
         }
+     var clipboardTextSingleItem = creatorsArray.join(', ');   
     }
+  clipboardText = clipboardText +  clipboardTextSingleItem + '\r\n';
 }
 
-var clipboardText = creatorsArray.join(', ');
-
-copyToClipboard(clipboardText)
-
+copyToClipboard(clipboardText);
 return clipboardText;
+
+
 
 function copyToClipboard(clipboardText) {
    if (clipboardText) {

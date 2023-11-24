89b2507b2e4b16a8fb6c172d7551252c07e86175
redleafnew <redleafnew@163.com>
2023-11-24 14:09:00 +0800

    Update 27remove-extra-space-in-abstract.js

--- 27remove-extra-space-in-abstract.js
+++ 27remove-extra-space-in-abstract.js
@@ -1,6 +1,7 @@
-
+// https://github.com/redleafnew/zotero-updateifsE/issues/124 需求来源
 var pattern = /\s{2,}/g
 var pattern1 = /\s(\d)/g
+var pattern2 = /(\d)\s(\/)/g;
 
 var items = Zotero.getActiveZoteroPane().getSelectedItems();
 
@@ -9,7 +10,8 @@ for (let item of items) {
     if (item.isRegularItem() && !item.isCollection()) {
 
         var newAb = item.getField('abstractNote').replace(pattern, ' ')
-            .replace(pattern1, "$1");  //新摘要
+            .replace(pattern1, "$1")
+			.replace(pattern2, "$1$2");  //新摘要
         item.setField('abstractNote', newAb);  //替换摘要
         item.saveTx();
 
